71a1462b6b1051d3d27d338254ab99fe9ea97772
FaithDaka <faithbusigu8@gmail.com>
2020-10-12 22:13:28 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import HeroSection from './components/Layout/HeroSection';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route> </Route>
+        <Route exact path="/" component={HeroSection} ></Route>
       </Switch>
     </BrowserRouter>
   )
---
+++ src/components/Layout/HeroSection.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <div className="container-fluid hero">
+            <div className="col-12">
+                <div className="hero-banner">
+                    <div className="img-container">
+                        <img src="/images/praise.jpg" />
+                        <div className="img-content">
+                            <h2 className="float-left head1">Kansanga</h2>
+                            <h2 className="float-left head2">Miracle Center</h2>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection;
